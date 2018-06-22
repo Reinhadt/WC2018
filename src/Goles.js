019c8a8883a624dc9d9686a83b03123f3135fd3d
unknown <omavaloss@gmail.com>
2018-06-22 09:56:13 -0500

    Ahora sin console.logs

--- src/Goles.js
+++ src/Goles.js
@@ -4,7 +4,7 @@ const Goles = (props) => {
     let goles = props.datos.filter( evento => {
         return evento.type_of_event === "goal" || evento.type_of_event === "goal-penalty" || evento.type_of_event === "goal-own"
     }).map( g => {
-        console.log(g)
+        //console.log(g)
         if(g.type_of_event === 'goal-own'){
             return(
                 <p>{g.player} <i>{g.time}</i> (Own-goal)</p>
@@ -14,8 +14,8 @@ const Goles = (props) => {
                 <p>{g.player} <i>{g.time}</i></p>
             )
         }
-        
-    })   
+
+    })
 
     return goles
 }
