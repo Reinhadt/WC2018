41512438a3fd31c55cff6266a373dfd3c45622d6
Omar <omavaloss@gmail.com>
2018-06-23 11:09:25 -0500

    Update Goles.js

--- src/Goles.js
+++ src/Goles.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Goles = (props) => {
     let goles = props.datos.filter( evento => {
-        return evento.type_of_event === "goal" || evento.type_of_event === "goal-own"
+        return evento.type_of_event === "goal" || evento.type_of_event === "goal-penalty"|| evento.type_of_event === "goal-own"
     }).map( g => {
         //console.log(g)
         if(g.type_of_event === 'goal-own'){
